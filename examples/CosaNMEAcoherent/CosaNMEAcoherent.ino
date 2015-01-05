65529246c7184b6f3338dad5da4720bf9cf07ca9
SlashDevin <slashdevin@not.really>
2015-01-04 23:25:38 -0500

    Separate fused and coherent examples
    Former NMEAfused example was really coherent.   Renamed and added simple
    fused example.

--- examples/CosaNMEAcoherent/CosaNMEAcoherent.ino
+++ examples/CosaNMEAcoherent/CosaNMEAcoherent.ino
@@ -28,6 +28,11 @@ UART uart1(1, &ibuf, &obuf);
 
 #endif
 
+#if defined(GPS_FIX_DATE) & !defined(GPS_FIX_TIME)
+// uncomment this to display just one pulse-per-day.
+//#define PULSE_PER_DAY
+#endif
+
 static uint32_t seconds = 0L;
 
 static NMEAGPS gps;
@@ -78,16 +83,46 @@ static void traceIt()
 
 static void sentenceReceived()
 {
-  // Accumulate all the reports
-  fused |= gps.fix();
+  // See if we stepped into a different time interval,
+  //   or if it has finally become valid after a cold start.
 
-  //  Print things out once per second
+  bool newInterval;
+#if defined(GPS_FIX_TIME)
+  newInterval = (gps.fix().valid.time &&
+                (!fused.valid.time ||
+                 (fused.dateTime.seconds != gps.fix().dateTime.seconds) ||
+                 (fused.dateTime.minutes != gps.fix().dateTime.minutes) ||
+                 (fused.dateTime.hours   != gps.fix().dateTime.hours)));
+#elif defined(PULSE_PER_DAY)
+  newInterval = (gps.fix().valid.date &&
+                (!fused.valid.date ||
+                 (fused.dateTime.date  != gps.fix().dateTime.date) ||
+                 (fused.dateTime.month != gps.fix().dateTime.month) ||
+                 (fused.dateTime.year  != gps.fix().dateTime.year)));
+#else
+  //  No date/time configured, so let's assume it's a new interval
+  //  if the seconds have changed.
   static uint32_t last_sentence = 0L;
   
-  if (last_sentence != seconds) {
-    last_sentence = seconds;
+  newInterval = (seconds != last_sentence);
+  last_sentence = seconds;
+#endif
 
+  if (newInterval) {
+
+    // Log the previous interval
     traceIt();
+
+    //  Since we're into the next time interval, we throw away
+    //     all of the previous fix and start with what we
+    //     just received.
+    fused = gps.fix();
+
+    gps.poll( &uart1, NMEAGPS::NMEA_GST );
+
+  } else {
+    // Accumulate all the reports in this time interval into a /coherent/ fix
+    fused |= gps.fix();
   }
 
 } // sentenceReceived
